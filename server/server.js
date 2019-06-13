40630803abbee6a09601218e67e25c7a1ba0857d
Ali Matroki <alimatroky@gmail.com>
2019-06-13 23:42:40 +0430

    try to solve heroku err

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 console.log(publicPath);
