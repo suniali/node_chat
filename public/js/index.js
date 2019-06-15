a70a11e89262bc0272857bdfd421a8ba42890c2d
Ali Matroki <alimatroky@gmail.com>
2019-06-15 20:35:41 +0430

    solve message bug & create message from JQuery

--- public/js/index.js
+++ public/js/index.js
@@ -14,6 +14,11 @@ socket.on('newEmail', function (email) {
 
 socket.on('newMessage', function (message) {
     console.log('new Message', message);
+
+    var li = jQuery('<li></li>');
+    li.text(message.from + " : " + message.text);
+
+    jQuery('#message').append(li);
 });
 
 socket.emit('createMessage', {
@@ -21,4 +26,14 @@ socket.emit('createMessage', {
     text: "I Love You Jarvis."
 }, function (data) {
     console.log('Got it.', data);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery("[name='message']").val()
+    }, function (data) {
+    });
 });
